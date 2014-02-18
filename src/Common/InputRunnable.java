1145883e6e77c42feeadfc8fb8c7a263aaf8e956
andy5mai <yourillino@hotmail.com>
2014-02-18 20:10:54 +0800

    change

--- src/Common/InputRunnable.java
+++ src/Common/InputRunnable.java
@@ -1,37 +1,69 @@
 package Common;
 
+import java.lang.reflect.Method;
 import java.util.Scanner;
 
 public class InputRunnable implements Runnable
 {
 	private Scanner scanner = new Scanner(System.in);
+	private Method methodNextInt;
+	
+	private InputRunnable() throws Exception
+	{
+		this.methodNextInt = Scanner.class.getDeclaredMethod("nextInt");
+	}
+	
+	public static InputRunnable getInputRunnable() throws Exception
+	{
+		InputRunnable inputRunnable = new InputRunnable();
+		return inputRunnable;
+	}
+	
 	@Override
 	public void run()
 	{
 		
 	}
 	
-	public int getInt(String str)
+//	public int getInt(String strInputHint)
+//	{
+//		return (Integer)this.get(strInputHint, this.methodNextInt);
+//	}
+	
+	public int getInt(String strInputHint, Object... objValidate)
 	{
-		Integer n;
+		return (Integer)this.get(strInputHint, this.methodNextInt, objValidate);
+	}
+	
+	public Object get(String strInputHint, Method methodGet, Object... objValidate)
+	{
+		Object obj;
 		do
 		{
 			try
 			{
-				System.out.print(str);
+				System.out.print(strInputHint);
+				obj = methodGet.invoke(this.scanner);
 				
-				n = this.scanner.nextInt();
+				if (objValidate.length == 0) break;
+				
+				if ((boolean)((Method)objValidate[0]).invoke(objValidate[1], obj))
+				{
+					System.out.println("this input is not correct !!! check failed!!!");
+					continue;
+				}
 			}
 			catch(Exception exc)
 			{
-				System.out.println("this input is not correct! ");
+				System.out.println("this input is not correct!");
 //				exc.printStackTrace();
-				n = null;
+				obj = null;
 				System.out.println("this scanner has next ? " + this.scanner.hasNext());
 				this.scanner.next();
 			}
-		}while(n == null);
+		}
+		while(obj == null);
 		
-		return n;
+		return obj;
 	}
 }
--- src/Test/Exec.java
+++ src/Test/Exec.java
@@ -1,23 +1,33 @@
 package Test;
 
+import java.lang.reflect.Method;
+
 import Common.InputRunnable;
 
 public class Exec
 {
-	public static void main(String[] args)
+	public static void main(String[] args) throws Exception
 	{
-		InputRunnable input = new InputRunnable();
+		InputRunnable input = InputRunnable.getInputRunnable();
 		Thread thread = new Thread(input);
 		thread.setDaemon(true);
 		thread.start();
 		
+		Method methodCheckRange = Exec.class.getDeclaredMethod("checkRange", Integer.class);
+		
 		do
 		{
-			input.getInt("Input Number 1 : ");
+			input.getInt("Input Number 1 : ", methodCheckRange, null);
 		}while(true);
 //		input.getInt("Input Number 2 : ");
 //		input.getInt("Input Number 3 : ");
 //		input.getInt("Input Number 4 : ");
 //		input.getInt("Input Number 5 : ");
+	}
+	
+	public static boolean checkRange(Integer nInput)
+	{
+		if (nInput > 5) return false;
+		return true;
 	}
 }
